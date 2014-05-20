f8d8f021d3dee6ce3a0c31639e5c1be5329559fd
Pontus Abrahamsson <pontus.abrahamsson@netrelations.se>
2014-05-20 10:18:39 +0200

    Create path.php

---
+++ path.php
@@ -0,0 +1,23 @@
+<?php 
+
+if( !defined('WP_LOAD_PATH') ) 
+{
+	/** classic root path if wp-content and plugins is below wp-config.php */
+	$classic_root = dirname(dirname(dirname(dirname(__FILE__)))) . '/' ;
+	
+	if( file_exists( $classic_root . 'wp-load.php') )
+	{
+		define( 'WP_LOAD_PATH', $classic_root );
+	}
+	else
+	{
+		if( file_exists( $path . 'wp-load.php') )
+		{
+			define( 'WP_LOAD_PATH', $path);
+		}
+		else
+		{
+			exit("Could not find wp-load.php");
+		}
+	}
+}
