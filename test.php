e3f0a8e920bcdb50d924f940f8fc125ce5f567f8
Pontus Abrahamsson <pontus.abrahamsson@netrelations.se>
2014-05-20 10:19:18 +0200

    Update test.php

--- test.php
+++ test.php
@@ -7,19 +7,8 @@
  * @since 0.9.9-dev
  */
 
-if ( !defined('WP_LOAD_PATH') ) {
-
-	/** classic root path if wp-content and plugins is below wp-config.php */
-	$classic_root = dirname(dirname(dirname(dirname(__FILE__)))) . '/' ;
-	
-	if (file_exists( $classic_root . 'wp-load.php') )
-		define( 'WP_LOAD_PATH', $classic_root);
-	else
-		if (file_exists( $path . 'wp-load.php') )
-			define( 'WP_LOAD_PATH', $path);
-		else
-			exit("Could not find wp-load.php");
-}
+// Load WordPress path
+require( 'path.php' );
 
 // let's load WordPress
 require_once( WP_LOAD_PATH . 'wp-load.php');
